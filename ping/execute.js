5b4a97bbcc89a16e1a35c046f82746b1d068dbba
Wasab <whitenoise09@free.fr>
2022-03-02 17:25:13 +0100

    Changes to ping

--- ping/execute.js
+++ ping/execute.js
@@ -3,7 +3,7 @@ const { client } = interaction;
 try {
     const message = await interaction.reply({ content: 'Pong !', fetchReply: true });
     console.log(message);
-    await interaction.followUp(message);
+    message.react('white_check_mark');
 } catch(e) {
     console.log(e);
     interaction.followUp('Error : ' + e.message);
